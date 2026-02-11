1c53d2b865bc5b47017ce7424dfd5622a83dda5c
Owen Werts <owen.werts@gmail.com>
2026-02-10 22:13:43 -0700

    Got first Playwright test working!

---
+++ tests/pizza.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from 'playwright-test-coverage';
+
+test('home page', async ({ page }) => {
+	await page.goto('/');
+
+	expect(await page.title()).toBe('JWT Pizza');
+});
+
+test('purchase with login', async ({ page }) => {
+	await page.goto('http://localhost:5173/');
+	await page.getByRole('link', { name: 'Login' }).click();
+	await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+	await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+	await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+	await page.getByRole('button', { name: 'Login' }).click();
+	await page.getByRole('button', { name: 'Order now' }).click();
+	await page.getByRole('combobox').selectOption('1');
+	await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+	await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+	await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+	await page.getByRole('button', { name: 'Checkout' }).click();
+	await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
+	await expect(page.locator('tbody')).toContainText('Veggie');
+	await expect(page.locator('tbody')).toContainText('Pepperoni');
+	await page.getByRole('button', { name: 'Pay now' }).click();
+	await expect(page.getByRole('main')).toContainText('2');
+	await expect(page.getByRole('main')).toContainText('0.008 ₿');
+});
